449d2ff846bfb85f5f8af24a0d1b12b535e48ca3
Lucas Landivar <landivarlucas67@gmail.com>
2025-02-24 20:55:40 -0300

    Creating & Using Async Validators

--- 01-starting-project/src/app/auth/login/login.component.ts
+++ 01-starting-project/src/app/auth/login/login.component.ts
@@ -68,6 +68,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -75,6 +76,14 @@ function mustContainQuestionMark(control: AbstractControl) {
   }
 
   return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null);
+  }
+
+  return of({ notUnique: true });
 }
 @Component({
   selector: 'app-login',
@@ -87,6 +96,7 @@ export class LoginComponent {
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
     }),
     password: new FormControl('', {
       validators: [
